4ac7048f9605ae8c93ec2a74a2249b424ebec1e1
W. Helga Alphandito Sitanggang <Helga_AS919@mitrais.com>
2020-06-24 02:09:30 +0700

    swagger done

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const mongoose = require("mongoose");
 const port = process.env.PORT_PRODUCTION || process.env.PORT_DEVELOPMENT;
 const todoRoutes = require('./routes');
 const uri = process.env.MONGO_PORT_PRODUCTION || "mongodb://localhost:27017/todoList_" + process.env.NODE_ENV;
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./swagger.json');
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useUnifiedTopology", true);
 mongoose.connect(uri, (err) => {
@@ -22,7 +20,6 @@ mongoose.connect(uri, (err) => {
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
-app.use(express.static('coverage'));
 app.use("/api", todoRoutes);
 app.use((err, req, res, next) => {
   let status = err.status || 500
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const todoRouter = require("./todos.routes");
 const TodoValidation = require("../middlewares/todo.validation");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
 router.use("/todos", todoRouter);
+router.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.use('*', TodoValidation.apiNotFound);
 
 module.exports = router
